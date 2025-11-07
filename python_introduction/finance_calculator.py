73c2ea80bc4a14fb47dc3f6328a7188d0f1be868
joe <joelpartey123@gmail.com>
2025-11-07 14:12:49 -0800

    finance calculator in python

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,4 @@
+monthly_income = input('Enter your monthly income')
+monthly_expenses = input('Enter your total monthly expenses')
+monthly_savings = int(monthly_income) - int(monthly_expenses)
+print(monthly_savings)
