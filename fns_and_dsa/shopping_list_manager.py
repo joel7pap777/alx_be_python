e4440baecaefbdc64cc0d142ce5b1606b311f593
joe <joelpartey123@gmail.com>
2025-11-25 11:53:52 -0800

    shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,26 +1,36 @@
-shopping_list = []
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
 
-while True:
- user_choice=  input('enter an option(1.add an item 2.remove an item 3.view list 4.exit):')
- if user_choice == '1':
-      user_input = input('what item do you want to add:')
-      shopping_list.append(user_input)
-      print(f"you have added {user_input} to shopping list")
- elif user_choice == '2':
+def main():
+ shopping_list = []
+ while True:
+     display_menu()
+     choice=  input('enter an option:')
+     if choice == '1':
+       user_input = input('what item do you want to add:')
+       shopping_list.append(user_input)
+       print(f"you have added {user_input} to shopping list")
+       
+     elif choice == '2':
        user_input = input('what item do you want to remove:')
        shopping_list.remove(user_input)
        print(f"you have removed {user_input} from shopping list")
        
- elif user_choice == '3':
-     print(shopping_list)
+     elif choice == '3':
+      print(shopping_list)
      
- elif user_choice == '4':
-     print('you have exited the application')
-     break
- else:
-     print('invalid input')
+     elif choice == '4':
+       print('Good bye!')
+       break
+   
+     else:
+      print('Invalid choice. Please try again.')
      
-     
+main() 
     
     
              
